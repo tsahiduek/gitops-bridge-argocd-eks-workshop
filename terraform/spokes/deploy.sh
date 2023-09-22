9f47cf4f5ca991ce8b2b12d05f07ffdfb144ca41
Tsahi <tsahiduek@gmail.com>
2023-09-22 17:21:58 +0100

    cange deploy script

--- terraform/spokes/deploy.sh
+++ terraform/spokes/deploy.sh
@@ -7,6 +7,8 @@ if [[ $# -eq 0 ]] ; then
     exit 1
 fi
 env=$1
+backend-config-bucket=$2
+backend-config-region=$3
 echo "Deploying $env with "workspaces/${env}.tfvars" ..."
 
 set -x
@@ -14,4 +16,7 @@ set -x
 terraform workspace new $env
 terraform workspace select $env
 terraform init
+terraform init --backend-config="bucket=${backend-config-bucket}" \
+--backend-config="key=${env}/terraform.tfstate" \
+--backend-config="region=${backend-config-region}"
 terraform apply -var-file="workspaces/${env}.tfvars"
